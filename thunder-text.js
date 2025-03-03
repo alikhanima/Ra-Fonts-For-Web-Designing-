c3ef21a978b56f6b1f5058b404daff59d71bd79c
alikhanima <alikhanbest10@gmail.com>
2025-03-03 06:03:29 +0500

    Update thunder-text.js

--- thunder-text.js
+++ thunder-text.js
@@ -9,12 +9,28 @@ class ThunderText {
             repeat: options.repeat || 3
         };
         
-        this.initEffect();
+        this.createLightningEffect();
     }
 
-    initEffect() {
+    createLightningEffect() {
         const el = this.element;
         const options = this.options;
+        el.style.position = "relative";
+        el.style.transition = "color 0.1s ease-in-out";
+        
+        // Create lightning overlay effect
+        const lightning = document.createElement("div");
+        lightning.style.position = "absolute";
+        lightning.style.top = "50%";
+        lightning.style.left = "50%";
+        lightning.style.transform = "translate(-50%, -50%)";
+        lightning.style.width = "120%";
+        lightning.style.height = "150%";
+        lightning.style.pointerEvents = "none";
+        lightning.style.background = `radial-gradient(circle, ${options.color} 10%, transparent 70%)`;
+        lightning.style.opacity = "0";
+        lightning.style.filter = `blur(${options.intensity / 5}px)`;
+        el.appendChild(lightning);
 
         const lightningEffect = () => {
             const tl = gsap.timeline({ repeat: options.repeat, repeatDelay: Math.random() * 0.2 });
@@ -25,22 +41,26 @@ class ThunderText {
                 duration: 0.05,
                 ease: "power2.out"
             })
+            .to(lightning, { opacity: options.flashOpacity, duration: 0.05 })
             .to(el, { 
                 textShadow: "none", 
                 color: "", 
                 duration: 0.1,
                 ease: "power2.in"
             })
+            .to(lightning, { opacity: 0, duration: 0.1 })
             .to(el, { 
                 textShadow: `0 0 ${options.intensity * 1.5}px ${options.color}, 0 0 ${options.intensity * 2.5}px ${options.color}`,
                 color: options.color,
                 duration: 0.05
             })
+            .to(lightning, { opacity: options.flashOpacity * 0.8, duration: 0.05 })
             .to(el, { 
                 textShadow: "none", 
                 color: "", 
                 duration: 0.1
-            });
+            })
+            .to(lightning, { opacity: 0, duration: 0.1 });
         };
 
         setInterval(lightningEffect, options.interval);
